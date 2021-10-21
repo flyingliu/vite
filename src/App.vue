7372149925f8ebe4757eabd10f828cb6546dbede
liufeilai <liufeilai@gmail.com>
2021-10-21 22:30:13 +0800

    21

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,5 @@
 <template>
   <router-view></router-view>
-  <!-- <div class="layout">
-    <main class="main">
-      <dl class="dl">
-        <dd class="page">
-          <header class="mheader"><h1>书法练习</h1></header>
-          <ul class="con">
-            <li v-for="(t, index) in con" :key="index" v-last>
-              <f-td :text="t"></f-td>
-            </li>
-          </ul>
-          <footer class="mfooter">
-            <sub>{{ new Date() }}</sub>
-          </footer>
-        </dd>
-      </dl>
-    </main>
-    <aside class="aside">
-      <h5 class="subtitle">打印设置</h5>
-      <ul class="sidelist">
-        <li>
-          <label class="label">内容设置：</label>
-          <div class="licon">
-            <f-input v-model:value="con" @change="changeCon"></f-input>
-          </div>
-        </li>
-        <li>
-          <label class="label">字体设置：</label>
-          <div class="licon">
-            <input type="file" accept="*" @change="upload" />
-            <select
-              v-model="selected"
-              @change="selectData($event.target.value)"
-            >
-              <option disabled value="">Please select one</option>
-              <option v-for="item in familylist" :value="item.value">{{
-                item.label
-              }}</option>
-            </select>
-          </div>
-        </li>
-      </ul>
-    </aside>
-  </div> -->
 </template>
 
 <script setup></script>
--- src/main.js
+++ src/main.js
@@ -1,30 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-// import fInput from './components/f-input.vue'
-// import fTd from './components/f-td.vue'
 
 import router from './router'
 const app = createApp(App)
 app.use(router)
-// app.component('f-input', fInput)
-// app.component('f-td', fTd)
-
-// app.directive('last', {
-//   mounted(el) {
-
-//     // Focus the element
-
-//     const text = el.innerText
-//     const eleft = el.offsetLeft
-//     const Arr = ['。', '，', '《', '》', '！', '？', ',', '.', '?', '!', '、']
-//     console.log(text, eleft, !eleft, Arr.includes(text))
-//     if (!eleft && Arr.includes(text)) {
-//       el.classList = ['isLast']
-//       console.log(text, eleft)
-//     }
-//   }
-
-// })
-
 app.mount('#app')
