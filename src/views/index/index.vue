51bada1b43c40b4e04fb0af41de23bd362b66243
liufeilai <liufeilai@gmail.com>
2021-10-26 00:39:07 +0800

    al

--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -1,18 +1,36 @@
 <template>
-  <div>
-    this.is home！
+  <div class="index">
+    <div class="mleft">
+      <h3 class="logo iconfont">&#xe611;&#xe607;&#xe608;&#xe610;</h3>
+      <h1>百日练字</h1>
+    </div>
 
-    <el-input v-model="input" placeholder="Please input" />
+    <dl>
+      <dd>
+        <i class="aicon iconfont">&#xe60d;</i>
+        <h3>作文对照临写</h3>
+        <p>可自己设置字体！</p>
+      </dd>
+      <dd>
+        <i class="aicon iconfont">&#xe60d;</i>
+        <h3>作文对照临写</h3>
+        <p>可自己设置字体！</p>
+      </dd>
+    </dl>
   </div>
-
 </template>
 
 <script setup>
 import { ref } from 'vue'
 const input = ref(10)
-
 </script>
 
-<style>
-
+<style lang="scss">
+.index {
+  display: flex;
+  gap: 20px;
+}
+.mleft {
+  width: 200px;
+}
 </style>
